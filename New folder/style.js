c1206505b9e1189f3fc99c9714c93cc9c4fa4bb6
rohan093 <108068790+rohan093@users.noreply.github.com>
2024-10-10 10:48:32 +0530

    Cold Play Ticket Booking
    cold play ticket booking frontend (html,css,javascript)

---
+++ New folder/style.js
@@ -0,0 +1,18 @@
+// document
+//   .getElementById("bookingForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevent the default form submission
+
+//     const concert = document.getElementById("concert").value;
+//     const ticketType = document.getElementById("ticketType").value;
+//     const ticketQuantity = document.getElementById("ticketQuantity").value;
+
+//     // Display confirmation message
+//     const confirmationMessage = document.getElementById("confirmationMessage");
+//     confirmationMessage.classList.remove("hidden");
+//     confirmationMessage.textContent = `Booking Confirmed!
+//         ${ticketQuantity} tickets for ${ticketType} at the ${concert} concert.`;
+
+//     // Reset the form
+//     this.reset();
+//   });
